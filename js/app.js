fc9697c81b5e2a7c0dab4d3763703a398ad3ec67
phonofidelic <chris_clemons85@hotmail.com>
2015-05-10 09:43:46 -0400

    Added map-tools lib, map constructor in DataModel'

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 // Model
 // GET requests responses are stored in variables here
 var DataModel = [
-	// google
+	// Google Map
 	{
-		url: 'https://www.google.com/maps/embed/v1/place?key=AIzaSyDsUk8JPHC9zfd3CLCEAk9kRVR9RpopZN4&q='
+		map: new mapTools({
+      id: 'mymap',
+      lat: 41.3833,
+      lng: 2.1833,
+      on: {
+        tilesloaded: function() {
+          console.log('tiles loaded')
+        }
+      },
+      scrollwheel: false,
+    }, function (err, map) {
+      if (!err) {
+        console.log('Map Loaded!', map.instance);
+      }
+    })
 	}
 
 	// wiki-data
@@ -14,14 +28,10 @@ var ViewModel = function() {
 	var self = this;
 
 	// Return google street view images based on search input
-	self.srcSubmit = function() {
+	this.srcSubmit = function() {
 		// get input from search form
-		this.srcInput = $('#src-form').value;
+		this.srcInput = $('#src-form').val;
     console.log(this.srcInput);
-
-		//get src string from DataModel
-		this.mapURL = DataModel[0].url + this.srcInput;
-    console.log('test');
 	};
 
   // Scroll down to map on click
@@ -38,41 +48,62 @@ var ViewModel = function() {
     });
   };
 
+  this.mapInit = function() {
+    map.instance;
+  };
+
+
 };
+
+// var googleMap = function() {
+//   var map = new mapTools({
+//     id: 'mymap',
+//     lat: 41.3833,
+//     lng: 2.1833
+//   }, function (err, map) {
+//     if (!err) {
+//       console.log('Map Loaded!', map.instance);
+//     }
+//   });
+// }
 
 
 // Scetch - initialize map and slides TODO: remove!!!!!!!!!!!!!!!!!
 
-//Google Maps API
-var geocoder = {};
-var map;
-function initialize() {
-  geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(0,0);
-  var mapOptions = {
-    zoom: 12,
-    center: latlng,
-    scrollwheel: false
-  }
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-};
+// //Google Maps API
+// var geocoder = {};
+// var map;
+// function initialize() {
+//   geocoder = new google.maps.Geocoder();
+//   var latlng = new google.maps.LatLng(0,0);
+//   var mapOptions = {
+//     zoom: 12,
+//     center: latlng,
+//     scrollwheel: false
+//   }
+//   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+// };
 
-function codeAddress() {
-  var address = document.getElementById('loc-src-input').value;
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-};
+// function codeAddress() {
+//   var address = document.getElementById('loc-src-input').value;
+//   geocoder.geocode( { 'address': address}, function(results, status) {
+//     if (status == google.maps.GeocoderStatus.OK) {
+//       map.setCenter(results[0].geometry.location);
+//       var marker = new google.maps.Marker({
+//           map: map,
+//           position: results[0].geometry.location
+//       });
+//     } else {
+//       alert('Geocode was not successful for the following reason: ' + status);
+//     }
+//   });
+// };
 
-google.maps.event.addDomListener(document.getElementById('search'), 'click', initialize);
+// google.maps.event.addDomListener(document.getElementById('search'), 'click', initialize);
+
+//!!!!!!!!! add onclick="codeAddress()" to search button in index.html!!!!!!!!!!!
+
+
 
 
 // Superslides API
