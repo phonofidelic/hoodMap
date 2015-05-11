da77bcdced63a0e178158f7d36b6a2af73b5fca8
phonofidelic <chris_clemons85@hotmail.com>
2015-05-11 09:28:58 -0400

    integrating google maps into ko's mvvm, map shows upp, getting error 'Google Maps Api included multiple times, may cause unexpected errors'

--- js/app.js
+++ js/app.js
@@ -2,10 +2,7 @@
 // GET requests responses are stored in variables here
 var DataModel = [
 	// Google Map
-  {
-    geocoder: {},
-    map: null
-  }
+
 
 
 
@@ -13,18 +10,29 @@ var DataModel = [
 ];
 
 // Controller
+
+//setup Google map object
 var GoogleMap = function() {
 
   this.initialize = function() {
-    DataModel.geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(0,0);
     var mapOptions = {
-      zoom: 12,
-      center: latlng,
+      center: new google.maps.LatLng(10, 10),
+      zoom: 2,
       scrollwheel: false
-    }
-    DataModel.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    };
+
+    var map = new google.maps.Map(document.getElementById('mymap'), mapOptions);
   };
+
+  this.loadScript = function() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
+      '&signed_in=false&callback=initialize'; //------------------------ set signed_in state to true or false
+    document.body.appendChild(script);
+  }
+
+  window.onload = this.loadScript;
 }
 
 var ViewModel = function() {
@@ -56,59 +64,21 @@ var ViewModel = function() {
     console.log('scrollDown')
   };
 
-  this.codeAddress = function() {
-    var address = document.getElementById('src-input').value;
-    DataModel.geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-  };
+  // this.codeAddress = function() {
+  //   var address = document.getElementById('src-input').value;
+  //   DataModel.geocoder.geocode( { 'address': address}, function(results, status) {
+  //     if (status == google.maps.GeocoderStatus.OK) {
+  //       map.setCenter(results[0].geometry.location);
+  //       var marker = new google.maps.Marker({
+  //           map: map,
+  //           position: results[0].geometry.location
+  //       });
+  //     } else {
+  //       alert('Geocode was not successful for the following reason: ' + status);
+  //     }
+  //   });
+  // };
 };
-
-
-// Scetch - initialize map and slides TODO: remove!!!!!!!!!!!!!!!!!
-
-// //Google Maps API
-// var geocoder = {};
-// var map;
-// function initialize() {
-//   geocoder = new google.maps.Geocoder();
-//   var latlng = new google.maps.LatLng(0,0);
-//   var mapOptions = {
-//     zoom: 12,
-//     center: latlng,
-//     scrollwheel: false
-//   }
-//   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-// };
-
-function codeAddress() {
-  var address = document.getElementById('loc-src-input').value;
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-};
-
-google.maps.event.addDomListener(document.getElementById('search'), 'click', GoogleMap.initialize);
-
-//!!!!!!!!! add onclick="codeAddress()" to search button in index.html!!!!!!!!!!!
-
-
 
 
 // Superslides API
@@ -116,4 +86,4 @@ $('#slides').superslides();
 
 
 // Initiate Knockout bindings
-ko.applyBindings(new ViewModel());
+ko.applyBindings(new ViewModel(), GoogleMap());
