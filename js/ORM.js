b62bdc9bb12f0b0cf8d4529b9de134c119c303f9
phonofidelic <chris_clemons85@hotmail.com>
2015-05-28 15:23:06 -0400

    Building ORM

---
+++ js/ORM.js
@@ -0,0 +1,25 @@
+var Model = {
+	inherited: function() {},
+	created: function() {},
+
+	prototype: {
+		init: function() {}
+	},
+
+	create: function() {
+		var object = Object.create(this);
+		object.parent = this;
+		object.prototype = object.fn = Object.create(this.prototype);
+
+		object.created();
+		this.inherited(object);
+		return object;
+	},
+
+	init: function() {
+		var instance = Object.create(this.prototype);
+		instance.parent = this;
+		instance.init.apply(instance, arguments);
+		return instance;
+	}
+};
