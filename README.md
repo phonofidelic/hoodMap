0dfc804ee2448e5d123e6252ef3b20b56e4b7e2a
phonofidelic <chris_clemons85@hotmail.com>
2015-08-07 12:58:37 -0400

    Added gulp processes and dist directory

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # hoodMap
-Udacity front-end wed dev Nanodegree - Neigborhood Map Project
-
+###Udacity front-end wed dev Nanodegree - Neigborhood Map Project
+Click here to view a live demo: https://phonofidelic.github.io/hoodMap_dist/index.html
 ## Usage:
 Enter a location you would like to explore into the search prompt. Click the 'find stuf' tab to look through a list of results, narrow down the list by entering more specific seasrch parameters lick 'coffee' or 'bars' etc. Click on a list item to have it highlighted on the map. You can also clock on the map icons for more details. Scroll down to see a more detailed list of results featureing Google Street View images and links to Yelp pages.
 
 ## Scources and help from:
 
-### jQuery scroll on click function:
+* jQuery scroll on click function:
 http://stackoverflow.com/a/6677069/4677401
 
-### Access-Control-Allow-Origin error sending a jQuery Post to Google (Yelp) API's
+* Access-Control-Allow-Origin error sending a jQuery Post to Google (Yelp) API's
 http://stackoverflow.com/a/6120260/4677401
 
-### List filtering:
-https://discussions.udacity.com/t/problems-with-filtering-list-view/22699?u=christopher_77363758
+* List filtering:
+https://discussions.udacity.com/t/problems-with-filtering-list-view
--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,74 @@
-var gulp = require('gulp'),
-  concat = require('gulp-concat'),
-  catCss = require('gulp-concat-css')
-  uglify = require('gulp-uglify'),
-  minCss = require('gulp-minify-css'),
-    maps = require('gulp-sourcemaps'),
-     del = require('del');
+var    gulp = require('gulp'),
+     concat = require('gulp-concat'),
+     catCss = require('gulp-concat-css'),
+     uglify = require('gulp-uglify'),
+     minCss = require('gulp-minify-css'),
+        del = require('del'),
+   validate = require('gulp-jsvalidate'),
+     rename = require('gulp-rename'),
+htmlReplace = require('gulp-html-replace');
 
-gulp.task('concatScripts', function() {
+// gulp.task('default', [/* 'task', 'task' */]);
+
+gulp.task('val', function() {
+	gulp.src('js/main.js')
+	.pipe(validate());
+});
+
+//process head scripts
+gulp.task('headScripts', function() {
+	gulp.src([
+		'bower_components/offline/offline.min.js',
+		'bower_components/map-icons/js/map-icons.js',
+		'bower_components/oauth-signature/dist/oauth-signature.js'
+		])
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('dist/js'))
+	.pipe(gulp.dest('../phonofidelic/phonofidelic.github.io/hoodMap_dist/js'));
+});
+
+//process scripts
+gulp.task('scripts', function() {
 	gulp.src([
 		'bower_components/jquery/dist/jquery.js',
+		'bower_components/sticky/jquery.sticky.js',
 		'bower_components/knockout/dist/knockout.js',
 		'bower_components/bootstrap/dist/js/bootstrap.js',
 		'js/main.js'
 		])
-	.pipe(maps.init())
-	.pipe(concat('app.js'))
-	.pipe(maps.write('./'))
-	.pipe(gulp.dest('../phonofidelic/phonofidelic.github.io/hoodMap_dist/js'));
-});
-
-gulp.task('miniJs', function() {
-	gulp.src('../phonofidelic/phonofidelic.github.io/hoodMap_dist/js/app.js')
+	.pipe(concat('app.min.js'))
 	.pipe(uglify())
+	.pipe(gulp.dest('dist/js'))
 	.pipe(gulp.dest('../phonofidelic/phonofidelic.github.io/hoodMap_dist/js'));
 });
 
-gulp.task('concatCss', function() {
+//process css
+gulp.task('appCss', function() {
 	gulp.src([
-		'bower_components/bootstrap/dist/css/bootstrap.css',
-		'Map-Icons-master/css/map-icons.css',
-		'css/style.css'
+		'./bower_components/bootstrap/dist/css/bootstrap.css',
+		'./bower_components/map-icons/css/map-icons.css',
+		'./bower_components/offline/themes/offline-language-english.css',
+		'./bower_components/offline/themes/offline-language-english-indicator.css',
+		'./bower_components/offline/themes/offline-theme-default.css',
+		'./css/style.css'
 		])
-	.pipe(maps.init())
-	.pipe(catCss('application.css'))
-	.pipe(maps.write('./'))
+	.pipe(catCss('application.min.css', {rebaseUrls:false}))
+	.pipe(minCss({rebase:false}))
+	.pipe(gulp.dest('dist/css'))
 	.pipe(gulp.dest('../phonofidelic/phonofidelic.github.io/hoodMap_dist/css'));
 });
 
-//!!! not working - img and font paths being altered
-gulp.task('miniCss', function() {
-	gulp.src('css/application.css')
-	.pipe(minCss())
-	.pipe(gulp.dest('css'));
+//process html
+gulp.task('htmlIndex', function(){
+	gulp.src('index.html')
+	.pipe(htmlReplace({
+		'appCss': 'css/application.min.css',
+		'headScripts': 'js/scripts.min.js',
+		'appScripts': 'js/app.min.js'
+	}))
+	.pipe(gulp.dest('dist'))
+	.pipe(gulp.dest('../phonofidelic/phonofidelic.github.io/hoodMap_dist'));
 });
+
+gulp.task('default', ['headScripts', 'scripts', 'appCss', 'htmlIndex']);
